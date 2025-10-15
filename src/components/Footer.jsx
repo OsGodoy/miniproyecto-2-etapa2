acb1cbf13be3d1c849ac62cd997dbd3acac51b7b
Pedro Godoy <pedrosvaldogodoy@gmail.com>
2025-10-14 19:47:00 -0500

    2do cambio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <footer className="p-4 flex items-center justify-center">
+      <footer className="p-5 flex items-center justify-center">
         <div className="text-[#A09FB1] text-xs font-medium flex items-center justify-center">
           OsGodoy-2025
         </div>
